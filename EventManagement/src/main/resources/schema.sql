ef5f6bcff5d7cee1083bf8d7b1d28fd90d59cf5d
craitbla <ki225431@gmail.com>
2025-10-20 15:25:13 +0300

    before connection db

---
+++ EventManagement/src/main/resources/schema.sql
@@ -0,0 +1,47 @@
+CREATE TABLE passports
+(
+    passport_id SERIAL PRIMARY KEY,
+    series      VARCHAR(4) NOT NULL,
+    number      VARCHAR(6) NOT NULL,
+    created_at  TIMESTAMP DEFAULT NOW(),
+    UNIQUE (series, number)
+);
+
+CREATE TABLE clients
+(
+    client_id    SERIAL PRIMARY KEY,
+    full_name    TEXT           NOT NULL,
+    phone_number VARCHAR(12)    NOT NULL CHECK (phone_number LIKE '+7%'),
+    email        TEXT           ,
+    passport_id  INTEGER UNIQUE NOT NULL REFERENCES passports (passport_id),
+    created_at   TIMESTAMP DEFAULT NOW(),
+    updated_at   TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE events
+(
+    event_id     SERIAL PRIMARY KEY,
+    name         TEXT NOT NULL,
+    date         DATE NOT NULL                                                                 DEFAULT CURRENT_DATE,
+    ticket_price NUMERIC NOT NULL                                                                       DEFAULT 0 CHECK (ticket_price >= 0),
+    status       TEXT CHECK (status IN ('запланировано', 'проходит', 'отменено', 'завершено')) DEFAULT 'запланировано',
+    description  TEXT,
+    created_at   TIMESTAMP                                                                     DEFAULT NOW(),
+    updated_at   TIMESTAMP                                                                     DEFAULT NOW()
+);
+
+CREATE TABLE ticket_reservations
+(
+    reservation_id    SERIAL PRIMARY KEY,
+    client_id         INTEGER REFERENCES clients (client_id),
+    event_id          INTEGER REFERENCES events (event_id),
+    number_of_tickets INTEGER   DEFAULT 1 CHECK (number_of_tickets > 0),
+    booking_status    TEXT CHECK (booking_status IN ('подтверждено', 'отменено', 'ожидает подтверждения'))  DEFAULT 'ожидает подтверждения',
+    created_at        TIMESTAMP DEFAULT NOW(),
+    updated_at        TIMESTAMP DEFAULT NOW()
+);
+
+--для тестов
+-- CREATE INDEX idx_reservations_client_event ON ticket_reservations(client_id, event_id);
+-- CREATE INDEX idx_events_date_status ON events(date, status);
+-- CREATE INDEX idx_clients_phone ON clients(phone_number);
